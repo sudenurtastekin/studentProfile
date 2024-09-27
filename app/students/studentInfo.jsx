3e766c69c687a1d61f58b114a80c90e53fa5ce9f
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-09-27 16:02:07 +0300

    yazı düzenlendi

--- app/students/studentInfo.jsx
+++ app/students/studentInfo.jsx
@@ -4,8 +4,9 @@ export default function StudentInfo({ student }) {
   }
 
   return (
+
     <div className="student-info-container">
-      <h2 className="student-info-title">Öğrenci Bilgileri</h2>
+      <h2 className="student-info-title"> Öğrenci Bilgileri </h2>
       <p className="student-info-item"><strong>Öğrenci Adı:</strong> <span>{student.name}</span></p>
       <p className="student-info-item"><strong>Öğrenci Numarası:</strong> <span>{student.studentNumber}</span></p>
       <p className="student-info-item"><strong>Email:</strong> <span>{student.email}</span></p>
